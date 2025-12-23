3d344ccb6dc8731d0c96fa0654c07b41126cc939
girish-kumar-sain <119300691+girish-kumar-sain@users.noreply.github.com>
2025-12-24 02:34:46 +0530

    Update views.py

--- views.py
+++ views.py
@@ -17,3 +17,21 @@ def submit(request, course_id):
                 )
 
         return redirect('show_exam_result', course_id=course_id)
+
+
+def show_exam_result(request, course_id):
+    submissions = Submission.objects.filter(user=request.user)
+    total_score = 0
+    score = 0
+
+    for submission in submissions:
+        total_score += submission.question.grade
+        if submission.choice.is_correct:
+            score += submission.question.grade
+
+    context = {
+        'score': score,
+        'total': total_score,
+        'passed': score >= total_score * 0.5
+    }
+    return render(request, 'onlinecourse/exam_result.html', context)
